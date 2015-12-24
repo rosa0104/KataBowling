76c937d999f473e0c558aa33351cb64d7b51c710
maja <krause.maja@yahoo.de>
2015-12-24 12:16:33 +0100

    Implement strike
    * Extract method to evaluate int value of a sub string
    * Implement calculation of strike rolls

--- src/KataBowling.java
+++ src/KataBowling.java
@@ -1,6 +1,3 @@
-/**
- * Created by Thiemo on 19.12.2015.
- */
 public class KataBowling {
     public static int calc(String s) {
         // initialize result with 0 to sum up the rolls
@@ -8,6 +5,10 @@ public class KataBowling {
         for(int i = 0; i < s.length(); i++){
             //for every single roll evaluate value
             String current = s.substring(i,i+1);
+            if (isOneOfTheLastTwoRollsAndPreviousWasStrike(s,i)){
+                // continue means jump directly to the next loop iteration and ignore switch(case
+                continue;
+            }
             switch (current){
                 case "-": break;
                 //in case of spare call spare method and add result to result variable
@@ -15,9 +16,14 @@ public class KataBowling {
                     result = result + spare(s, i);
                     break;
                 }
+                case "X": {
+                    result = result + strike(s, i);
+                    break;
+
+                }
                 default: {
                     //result++ begin from 0, except it is the bonus roll (previous roll was last frame of the game and spare-->method)
-                    if ( i == 0 || isLastRollAndPreviousWasSpare(s, i)){
+                    if ( i == 0 || !isLastRollAndPreviousWasSpare(s, i)){
                         result = result + Integer.parseInt(current);
                     }
                 }
@@ -27,27 +33,55 @@ public class KataBowling {
         //return summed up rolls
         return result;
     }
+
+    //returns true, iff i is one of the last two rolls of the game and third-last roll of the game was a strike
+    private static boolean isOneOfTheLastTwoRollsAndPreviousWasStrike(String s, int i) {
+    return (s.substring(s.length()-3, s.length() - 2).equals("X")&& i >= s.length()-2);
+    }
+
     //returns true, iff(if and only if) previous roll was spare and current roll is the last roll of the game
     private static boolean isLastRollAndPreviousWasSpare(String s, int i) {
-        return !(s.substring(i-1, i).equals("/")&& i == s.length()-1);
+        return (s.substring(i-1, i).equals("/")&& i == s.length()-1);
     }
+    private static int strike(String s, int i) {
+        int currentValue = 10;
+        //look to result next rolls, sum = result strike value
+        String next = s.substring(i+1, i+2);
+        int nextValue = evaluateIntegerValue(next);
+        String nextNext = s.substring(i+2, i+3);
+        int nextNextValue = evaluateIntegerValue(nextNext);
+        return currentValue + nextValue + nextNextValue;
+    }
+
+
 
     private static int spare(String s, int i) {
         //look to result previous roll, 10 - result
         String previous = s.substring(i - 1, i);
 
-        int previousValue;
-        if (previous.equals("-")){
-            //- = 0 cannot be parsed
-            previousValue = 0;
-        }else {
-            previousValue = Integer.parseInt(previous);
-        }
+        int previousValue = evaluateIntegerValue(previous);
 
         int currentValue = 10 - previousValue;
         //look to result next roll, sum = result spare value
         String next = s.substring(i+1, i+2);
-        int nextValue = Integer.parseInt(next);
-        return currentValue + nextValue;
+        //evaluate IntegerValue of String Next (außerhalb der Klammer wird die Variable deklariert)
+        int nextValue = evaluateIntegerValue(next);
+            return currentValue + nextValue;
+    }
+
+    private static int evaluateIntegerValue(String stringValue) {
+        int integerValue;
+        switch (stringValue) {
+            case "-":
+                integerValue = 0;
+                break;
+            case "X":
+                integerValue = 10;
+                break;
+            default:
+                integerValue = Integer.parseInt(stringValue);
+                break;
+        }
+        return integerValue;
     }
 }
